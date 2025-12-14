03d5ee9a693e0f95862c60c38dacecb13c07b7c6
Aura SA <aurasukma.auliaa@gmail.com>
2025-12-14 22:50:49 +0700

    Update ProductGrid.jsx

--- src/components/organisms/ProductGrid/ProductGrid.jsx
+++ src/components/organisms/ProductGrid/ProductGrid.jsx
@@ -4,7 +4,7 @@ export default function ProductGrid({ products, onAdd }) {
   return (
     <div className="grid w-full pt-1 grid-cols-2 md:grid-cols-4 gap-6">
       {products.map((p) => (
-        <ProductCard key={p.id} product={p} onAdd={onAdd} />
+        <ProductCard key={p._id} product={p} onAdd={onAdd} />
       ))}
     </div>
   );
